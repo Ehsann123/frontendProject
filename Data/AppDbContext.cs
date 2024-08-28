0c65936c073c6a0c5479f53771e2412dc3dc5064
Ehsann123 <ehsanssb@gmail.com>
2024-08-28 22:49:04 +0500

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using frontendProject.Models;
+
+namespace frontendProject.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
